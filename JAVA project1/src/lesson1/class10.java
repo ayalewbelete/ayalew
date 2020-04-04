9872098d69660f2b8d9d4482bb29ae88b1c0f3e2
ayalewbelete <62919273+ayalewbelete@users.noreply.github.com>
2020-04-04 00:15:23 -0600

    final commit

--- JAVA project1/src/lesson1/class10.java
+++ JAVA project1/src/lesson1/class10.java
@@ -4,7 +4,7 @@ public class class10 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("hey there");
+System.out.println("hello there");
 	}
 
 }
